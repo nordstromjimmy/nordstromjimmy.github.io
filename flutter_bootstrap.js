bc217df88306ea2ae4f8389a0372ee88846b7a0b
nordstromjimmy <nordstromjimmy@gmail.com>
2024-10-02 20:13:44 +0200

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"207f088f73cb00fa05aff170030df891d62cad
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3849712065"
+    serviceWorkerVersion: "1246689843"
   }
 });
